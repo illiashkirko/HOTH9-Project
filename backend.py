144694ad3490563def0bb5fff78e673c4278eca6
Illia Shkirko <illiashkirko@g.ucla.edu>
2022-02-26 16:07:43 -0800

    all options included

--- backend.py
+++ backend.py
@@ -1,18 +1,38 @@
-import requests
-from bs4 import BeautifulSoup
+def filteroptions(myinput):
+    food_types = {
+        "vegetarian" : "/Content/Images/WebCodes/128px/v.png",
+        "vegan" : "/Content/Images/WebCodes/128px/vg.png",
+        "contains_peanuts" : "/Content/Images/WebCodes/128px/apnt.png",
+        "contains_tree_nuts" : "/Content/Images/WebCodes/128px/atnt.png",
+        "contains_wheat" : "/Content/Images/WebCodes/128px/awht.png",
+        "contains_gluten" : "/Content/Images/WebCodes/128px/agtn.png",
+        "contains_soy" : "/Content/Images/WebCodes/128px/asoy.png",
+        "contains_dairy" : "/Content/Images/WebCodes/128px/amlk.png",
+        "contains_eggs" : "/Content/Images/WebCodes/128px/aegg.png",
+        "contains_crustacean_shellfish" : "/Content/Images/WebCodes/128px/acsf.png",
+        "contains_fish" : "/Content/Images/WebCodes/128px/afsh.png",
+        "hahal_menu_option" : "/Content/Images/WebCodes/128px/hal.png",
+        "low_carbon_footprint" : "/Content/Images/WebCodes/128px/lc.png",
+        "high_carbon_footprint" : "/Content/Images/WebCodes/128px/hc.png"
+    }
 
-URL = "https://menu.dining.ucla.edu/Menus"
-page = requests.get(URL)
+    import requests
+    from bs4 import BeautifulSoup
 
-soup = BeautifulSoup(page.content, "html.parser")
-entire_page = soup.find(id="main-content")
-food_items = entire_page.find_all("li", class_="menu-item")
+    URL = "https://menu.dining.ucla.edu/Menus"
+    page = requests.get(URL)
 
-for food_item in food_items:
-    title = food_item.find("a")
-    icons = food_item.find_all("img")
-    for img in icons:
-        img_source = img["src"]
-        if img_source== "/Content/Images/WebCodes/128px/v.png":
-            print(title.text + " is vegeterian!")
-            break
+    soup = BeautifulSoup(page.content, "html.parser")
+    entire_page = soup.find(id="main-content")
+    food_items = entire_page.find_all("li", class_="menu-item")
+    filter = food_types[myinput]
+    result = []
+    for food_item in food_items:
+        title = food_item.find("a")
+        icons = food_item.find_all("img")
+        for img in icons:
+            img_source = img["src"]
+            if img_source == filter:
+                result.append(title.text)
+                break
+    return result
